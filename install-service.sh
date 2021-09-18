f8d6527cfe13e9873ea53ce64a54e65bf0ab5dd2
Joshua Luce <joshuabluce@protonmail.com>
2021-09-18 00:22:10 -0500

    Some fixes

--- install-service.sh
+++ install-service.sh
@@ -4,9 +4,8 @@
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 
 ENTRY=$(basename $(pwd))
-ROOTPATH=$(pwd)
 TIMERVAL=$1
 
-cat $SCRIPT_PATH/service-template | sed -e "s|%ENTRY%|$ENTRY|g" -e "s|%ROOTPATH%|$ROOTPATH|g" > /etc/systemd/system/restic-$ENTRY.service
-cat $SCRIPT_PATH/timer-template | sed -e "s|%ENTRY%|$ENTRY|g" -e "s|%ROOTPATH%|$ROOTPATH|g" -e "s|%CALENDARENTRY%|$TIMERVAL|g" > /etc/systemd/system/restic-$ENTRY.timer
+cat $SCRIPT_PATH/service-template | sed -e "s|%ENTRY%|$ENTRY|g" -e "s|%ROOTPATH%|$SCRIPT_PATH|g" > /etc/systemd/system/restic-$ENTRY.service
+cat $SCRIPT_PATH/timer-template | sed -e "s|%ENTRY%|$ENTRY|g" -e "s|%ROOTPATH%|$SCRIPT_PATH|g" -e "s|%CALENDARENTRY%|$TIMERVAL|g" > /etc/systemd/system/restic-$ENTRY.timer
 
